4a4e7c036b94336fd2b628b3257ac2f104750dac
betobye <betobye@gmail.com>
2025-08-29 23:09:06 -0300

    Aula 2908

---
+++ Tipos.py
@@ -0,0 +1,14 @@
+var = 1
+var2 = 10.4
+var = 15.2
+
+nome = "Leonardo" + "10"
+
+booleano = True
+booleano = False
+
+Lista = ["macarrao", "pao", "arroz", "molho"]
+lista2 = [1,2,3,4]
+
+resultado = var + var2
+print(resultado)
