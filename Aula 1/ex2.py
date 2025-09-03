77b62388c9be641542b42f203c8e31b8eb8476af
betobye <betobye@gmail.com>
2025-09-02 22:13:23 -0300

    Aula 0209

---
+++ Aula 1/ex2.py
@@ -0,0 +1,15 @@
+nome = "Roberto Batista de Oliveira"
+data_nascimento = "30/11/1973"
+cidade = "São Paulo"
+cpf = 14244499873
+sexo_masculino = True
+
+print("Nome:", nome)
+print("Data de Nascimento:", data_nascimento)
+print("Cidade:", cidade)
+print("CPF", cpf)
+
+if sexo_masculino:
+    print ("Sexo: Masculino")
+else:
+    print ("Sexo: Feminino")
---
+++ Aula 1/ex3.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+c = 0
+
+c = a
+a = b
+b = c
+
+print("var a", a)
+print("var b", b)
+
+a2 = 30
+b2 = 40
+
+a2,b2 = b2,a2
