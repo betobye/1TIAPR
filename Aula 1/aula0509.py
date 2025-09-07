b23056026b33c215ae8bdc068f35c1015ffdf860
betobye <betobye@gmail.com>
2025-09-07 16:35:34 -0300

    Aula 0509

---
+++ Aula 1/aula0509.py
@@ -0,0 +1,10 @@
+numero = float(input("Digite um numero: "))
+print(numero)
+
+if numero > 0: 
+    print ("O numero é positivo")
+elif numero < 0:
+    print ("O numero é negativo")
+else:
+    print ("O numero é Zero")
+ 
