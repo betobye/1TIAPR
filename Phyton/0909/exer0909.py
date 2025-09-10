8ec74d68e915918b9278301acced1f1d9274cc93
betobye <betobye@gmail.com>
2025-09-09 21:56:24 -0300

    execicio0909

---
+++ Phyton/0909/exer0909.py
@@ -0,0 +1,14 @@
+preco = float(input("Entre com o preco do produto "))
+quantidade = float(input("Entre com o quantidade adquirida "))
+total = preco * quantidade
+print(total)
+if quantidade > 10:
+    total = total * 0.9
+print("Total a pagar : ", total)
+
+
+idade = float(input("Entre com sua idade "))
+if idade >= 16: 
+    print("Voce pode voltar")
+else:
+    print("Voce não vai voltar")
